c8365bb7c009b469d4c0dd9597f1367bafd6ad75
Philippe Dumonet <philippe@dumo.net>
2022-09-27 05:57:41 +0200

    Psuh initial version of GobblerThrone

---
+++ README.md
@@ -0,0 +1,36 @@
+# Gobbler Throne
+> No one person can bear the might of a legendary gobbler, we shall bear it together!
+
+`GobblerThrone` is a relatively simple pooling contract that can be deployed by
+anybody. It allows anybody to join a pool by depositing their revealed Gobblers
+in the hopes of minting a _Legendary Gobbler_ together.
+
+If the legendary mint is successful the almighty, freshly minted legendary Gobbler
+is placed upon the throne, out of all the participants' reach, accruing GOO, locked
+in place until the throne is set to be dissolved. In exchange for their
+sacrifice participants receive Gobbler Throne shares (or GOTTEM's).
+
+Once the throne is set to dissolve, the Legendary Gobbler is auctioned off for
+even more GOO. Once the auction is complete
+and the throne is fully dissolved shares can be redeemed for a proportional
+share of all the GOO that was accumulated in the throne contract.
+
+## Specifics
+### Share Receival
+Due to the decreasing price of legendary Gobblers the contract is first-come
+first-served meaning if there's 60 Gobblers in the contract but the legendary
+only costs 57, only the first 57 participants will be able to receive shares,
+the last 3 will however be able to retrieve their unused Gobblers from the
+throne contract (not including any accrued GOO).
+
+### Failure To Form
+If the throne contract doesn't accrue sufficient Gobblers by a certain time it
+can enter failure mode, allowing all participants to withdraw their Gobblers.
+The time until the failure mode can be activated is set upon deployment.
+
+### Auction
+The final auction is also set to begin at a certain time after throne creation
+depending on the configured delay. The auction must go for at least 30 minutes
+without bids before finalizing, this is to protect against last minute sniping
+whereby people wait until the last moment to submit bids. Each bid must be at
+least 5% higher than the previous.
---
+++ src/GobblerThrone.sol
@@ -0,0 +1,215 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity 0.8.15;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ERC20} from "solmate/tokens/ERC20.sol";
+import {IArtGobblers} from "./IArtGobblers.sol";
+
+/// @author Philippe Dumonet <philippe@dumo.net>
+/// @author Philogy <https://github.com/Philogy>
+contract GobblerThrone is ERC20 {
+    struct Participant {
+        address account;
+        uint16 gobblerId;
+        uint8 multiple;
+    }
+    // each uint16 is one deposited gobbler:
+    // <2 bits - multiple> <14 bits - gobbler ID>
+    mapping(uint256 => Participant) internal participants;
+
+    uint256 internal constant FIRST_LEGENDARY_GOBBLER_ID = 9991;
+    // 1000 shares / 1x gobbler multiple
+    uint256 internal constant MULTIPLE_TO_SHARES_FACTOR = 1e21;
+
+    // 1 GOO
+    uint128 internal constant AUCTION_START_PRICE = 1e18;
+    uint256 internal constant ONE_IN_BPS = 10000; // 5%
+    uint256 internal constant BID_MIN_INCREASE_BPS = 500; // 5%
+    uint256 internal constant BID_MIN_INCREASE_FACTOR_BPS =
+        ONE_IN_BPS + BID_MIN_INCREASE_BPS; // x1.05
+    uint256 internal constant AUCTION_GRACE_PERIOD = 30 minutes;
+    uint256 internal constant AUCTION_START_DURATION = 7 days;
+
+    enum ThroneState {
+        Forming,
+        FormationFailed,
+        Formed,
+        AuctionInProgress,
+        Dissolved
+    }
+
+    ThroneState internal state;
+    uint16 public totalJoined;
+    uint16 public totalParticipants;
+    uint16 public legendaryGobblerId;
+
+    // auction data
+    uint64 public auctionEnd;
+    uint128 public bestBid;
+    address public topBidder;
+
+    uint256 public immutable auctionStartsAt;
+    uint256 public immutable formationFailsAt;
+    IArtGobblers public immutable artGobblers;
+    IERC20 public immutable goo;
+
+    error WrongState();
+    error UnacceptableGobbler();
+    error SharesAlreadyIssued();
+    error NotParticipant();
+    error ParticipantCannotExit();
+    error InsufficientGobblers();
+    error BidTooLow();
+
+    constructor(
+        address _artGobblers,
+        uint256 _maxFormationTime,
+        uint256 _totalLifeTime
+    ) ERC20("Gobbler Throne Shares", "GOT", 18) {
+        assert(_maxFormationTime < _totalLifeTime);
+        auctionStartsAt = block.timestamp + _totalLifeTime;
+        formationFailsAt = block.timestamp + _maxFormationTime;
+        artGobblers = IArtGobblers(_artGobblers);
+        goo = IERC20(IArtGobblers(_artGobblers).goo());
+    }
+
+    modifier onlyState(ThroneState _expectedState) {
+        if (state != _expectedState) revert WrongState();
+        _;
+    }
+
+    function join(uint256 _gobblerId) external onlyState(ThroneState.Forming) {
+        artGobblers.transferFrom(msg.sender, address(this), _gobblerId);
+        unchecked {
+            uint256 emissionsMultiple = artGobblers.getGobblerEmissionMultiple(
+                _gobblerId
+            );
+            // abuse underflow for cheaper range check
+            if (
+                (emissionsMultiple - 6) >= 4 ||
+                _gobblerId >= FIRST_LEGENDARY_GOBBLER_ID
+            ) revert UnacceptableGobbler();
+
+            participants[totalJoined++] = Participant({
+                account: msg.sender,
+                gobblerId: uint16(_gobblerId),
+                multiple: uint8(emissionsMultiple)
+            });
+        }
+    }
+
+    function form() external onlyState(ThroneState.Forming) {
+        uint256 gobblersToBeSacrificed = artGobblers.legendaryGobblerPrice();
+        if (totalJoined < gobblersToBeSacrificed) revert InsufficientGobblers();
+        uint256[] memory gobblerIds = new uint256[](gobblersToBeSacrificed);
+        for (uint256 i; i < gobblersToBeSacrificed; ) {
+            gobblerIds[i] = participants[i].gobblerId;
+            // prettier-ignore
+            unchecked { i++; }
+        }
+        legendaryGobblerId = uint16(
+            artGobblers.mintLegendaryGobbler(gobblerIds)
+        );
+        totalParticipants = uint16(gobblersToBeSacrificed);
+        state = ThroneState.Formed;
+    }
+
+    function checkFailedFormation() external onlyState(ThroneState.Forming) {
+        if (block.timestamp >= formationFailsAt)
+            state = ThroneState.FormationFailed;
+    }
+
+    function failExit(uint256 _participantId)
+        external
+        onlyState(ThroneState.FormationFailed)
+    {
+        address account = participants[_participantId].account;
+        uint256 gobblerId = participants[_participantId].gobblerId;
+        delete participants[_participantId];
+        artGobblers.transferFrom(address(this), account, gobblerId);
+    }
+
+    function issueShares(uint256 _participantId)
+        external
+        onlyState(ThroneState.Formed)
+    {
+        if (_participantId >= totalParticipants) revert NotParticipant();
+        address account = participants[_participantId].account;
+        uint256 multiple = participants[_participantId].multiple;
+        if (account == address(0)) revert SharesAlreadyIssued();
+        delete participants[_participantId];
+        _mint(account, MULTIPLE_TO_SHARES_FACTOR * multiple);
+    }
+
+    function notParticipantExit(uint256 _participantId)
+        external
+        onlyState(ThroneState.Formed)
+    {
+        if (_participantId < totalParticipants) revert ParticipantCannotExit();
+        address account = participants[_participantId].account;
+        uint256 gobblerId = participants[_participantId].gobblerId;
+        delete participants[_participantId];
+        artGobblers.transferFrom(address(this), account, gobblerId);
+    }
+
+    function checkAuctionStart() external onlyState(ThroneState.Formed) {
+        if (block.timestamp >= auctionStartsAt) {
+            state = ThroneState.AuctionInProgress;
+            auctionEnd = uint64(block.timestamp + AUCTION_START_DURATION);
+            bestBid = AUCTION_START_PRICE;
+        }
+    }
+
+    function checkAuctionEnd()
+        public
+        onlyState(ThroneState.AuctionInProgress)
+        returns (bool)
+    {
+        if (block.timestamp > auctionEnd) {
+            state = ThroneState.Dissolved;
+            artGobblers.transferFrom(
+                address(this),
+                topBidder,
+                legendaryGobblerId
+            );
+            artGobblers.removeGoo(artGobblers.gooBalance(address(this)));
+            return true;
+        }
+        return false;
+    }
+
+    function bid(uint256 _bidAmount)
+        external
+        onlyState(ThroneState.AuctionInProgress)
+    {
+        goo.transferFrom(msg.sender, address(this), _bidAmount);
+        if (checkAuctionEnd()) return;
+        address cachedTopBidder = topBidder;
+        uint256 prevBestBid = bestBid;
+        if (prevBestBid * BID_MIN_INCREASE_FACTOR_BPS > _bidAmount * ONE_IN_BPS)
+            revert BidTooLow();
+        if (cachedTopBidder != address(0))
+            goo.transfer(cachedTopBidder, prevBestBid);
+        bestBid = uint128(_bidAmount);
+        topBidder = msg.sender;
+        uint256 minimumEndTime = block.timestamp + AUCTION_GRACE_PERIOD;
+        uint256 currentAuctionEnd = auctionEnd;
+        auctionEnd = uint64(
+            minimumEndTime > currentAuctionEnd
+                ? minimumEndTime
+                : currentAuctionEnd
+        );
+    }
+
+    function claimShare(address _recipient)
+        external
+        onlyState(ThroneState.Dissolved)
+    {
+        uint256 shares = balanceOf[msg.sender];
+        uint256 totalShares = totalSupply;
+        uint256 gooShare = (goo.balanceOf(address(this)) * shares) /
+            totalShares;
+        _burn(msg.sender, shares);
+        goo.transfer(_recipient, gooShare);
+    }
+}
