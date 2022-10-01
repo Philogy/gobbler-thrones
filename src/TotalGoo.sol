6ecfe59e12cd5e4a38f89f148f015279e22c3f31
philogy <philippe@dumo.net>
2022-09-30 19:48:23 -0500

    Separate VirtualGoo into total and virtual

--- src/TotalGoo.sol
+++ src/TotalGoo.sol
@@ -5,17 +5,19 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IArtGobblers} from "./IArtGobblers.sol";
 
 /// @author Philippe Dumonet <philippe@dumo.net>
-contract VirtualGoo {
-    string public constant name = "Virtual GOO";
-    string public constant symbol = "vGOO";
+contract TotalGoo {
+    string public constant name = "Total GOO (virtual + token)";
+    string public constant symbol = "tGOO";
 
     IArtGobblers public immutable artGobblers;
+    IERC20 public immutable goo;
 
     constructor(address _artGobblers) {
         artGobblers = IArtGobblers(_artGobblers);
+        goo = IERC20(IArtGobblers(_artGobblers).goo());
     }
 
     function balanceOf(address _addr) external view returns (uint256) {
-        return artGobblers.gooBalance(_addr);
+        return artGobblers.gooBalance(_addr) + goo.balanceOf(_addr);
     }
 }
