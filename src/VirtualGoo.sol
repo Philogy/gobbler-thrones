daa8c6e909d9daf30fc6ccc98cc804a9bcf1427c
philogy <philippe@dumo.net>
2022-09-30 19:46:42 -0500

    Create VirtualGoo helper contract

---
+++ src/VirtualGoo.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity 0.8.15;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IArtGobblers} from "./IArtGobblers.sol";
+
+/// @author Philippe Dumonet <philippe@dumo.net>
+contract VirtualGoo {
+    string public constant name = "Total GOO (virtual + token)";
+    string public constant symbol = "vGOO";
+
+    IArtGobblers public immutable artGobblers;
+    IERC20 public immutable goo;
+
+    constructor(address _artGobblers) {
+        artGobblers = IArtGobblers(_artGobblers);
+        goo = IERC20(IArtGobblers(_artGobblers).goo());
+    }
+
+    function balanceOf(address _addr) external view returns (uint256) {
+        return artGobblers.gooBalance(_addr) + goo.balanceOf(_addr);
+    }
+}
