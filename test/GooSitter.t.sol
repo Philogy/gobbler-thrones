0bfdeb1b75862ce20143d9f75260f142438f25ec
Philippe Dumonet <philippe@dumo.net>
2022-10-28 22:19:11 +0200

    Add tests

--- test/GooSitter.t.sol
+++ test/GooSitter.t.sol
@@ -14,29 +14,33 @@ contract GooSitterTest is Test {
     GooSitter sitter;
 
     address manager = vm.addr(1);
-    address user = vm.addr(2);
-    address attacker = vm.addr(3);
+    address owner = vm.addr(2);
 
     function setUp() public {
         goo = new MockERC20();
         gobblers = new MockArtGobblers(goo);
+        vm.prank(owner);
         sitter = new GooSitter(address(gobblers), address(goo), manager);
     }
 
-    function testFailNotManagerBuy(address _caller) external {
+    function testInitialOwner() public {
+        assertEq(sitter.owner(), owner);
+    }
+
+    function testFailNotManagerBuy(address _caller) public {
         vm.assume(_caller != manager);
         vm.prank(_caller);
         sitter.buyGobbler(100e18);
     }
 
-    function testFailCallValueBuy(uint256 _amount) external {
+    function testFailCallValueBuy(uint256 _amount) public {
         vm.assume(_amount > 0);
         vm.deal(manager, _amount);
         vm.prank(manager);
         sitter.buyGobbler{value: _amount}(100e18);
     }
 
-    function testManagerCanBuy() external {
+    function testManagerCanBuy() public {
         uint256 maxPrice = 3000e18;
         vm.prank(manager);
         vm.expectCall(
@@ -44,5 +48,47 @@ contract GooSitterTest is Test {
             abi.encodeCall(IArtGobblers.mintFromGoo, (maxPrice, true))
         );
         sitter.buyGobbler(maxPrice);
+    }
+
+    function testGooConsolidation(uint256 _mintAmount) public {
+        goo.mint(address(sitter), _mintAmount);
+        sitter.consolidateGoo();
+        assertEq(gobblers.gooBalance(address(sitter)), _mintAmount);
+        assertEq(goo.balanceOf(address(sitter)), 0);
+    }
+
+    function testFailNotOwnerWithdraw(address _withdrawer) public {
+        vm.assume(_withdrawer != owner);
+        vm.prank(_withdrawer);
+        sitter.withdraw(_withdrawer, new uint256[](0));
+    }
+
+    function testOwnerWithdraw(address _recipient, uint256 _gooAmount) public {
+        vm.assume(_recipient != address(0));
+
+        // setup Gobblers
+        uint256[] memory tokens = new uint256[](5);
+        for (uint256 i; i < 5; ) {
+            tokens[i] = gobblers.mint(address(sitter));
+            assertEq(gobblers.ownerOf(tokens[i]), address(sitter));
+            // prettier-ignore
+            unchecked { ++i; }
+        }
+
+        // setup GOO
+        goo.mint(address(sitter), _gooAmount);
+        sitter.consolidateGoo();
+        assertEq(gobblers.gooBalance(address(sitter)), _gooAmount);
+        assertEq(goo.balanceOf(address(sitter)), 0);
+
+        vm.prank(owner);
+        sitter.withdraw(_recipient, tokens);
+
+        for (uint256 i; i < 5; ) {
+            assertEq(gobblers.ownerOf(tokens[i]), _recipient);
+            // prettier-ignore
+            unchecked { ++i; }
+        }
+        assertEq(goo.balanceOf(_recipient), _gooAmount);
     }
 }
--- test/mock/MockArtGobblers.sol
+++ test/mock/MockArtGobblers.sol
@@ -1,17 +1,19 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity 0.8.15;
 
+import {ERC721} from "solmate/tokens/ERC721.sol";
 import {MockERC20} from "./MockERC20.sol";
 
 /// @author Philippe Dumonet <https://github.com/philogy>
-contract MockArtGobblers {
+contract MockArtGobblers is ERC721 {
     uint256 __someState;
 
     MockERC20 internal immutable goo;
 
     mapping(address => uint256) public gooBalance;
+    uint256 public totalSupply;
 
-    constructor(MockERC20 _goo) {
+    constructor(MockERC20 _goo) ERC721("Mock ArtGobblers", "MAGOO") {
         goo = _goo;
     }
 
@@ -23,6 +25,16 @@ contract MockArtGobblers {
     function removeGoo(uint256 _amount) external {
         goo.mint(msg.sender, _amount);
         gooBalance[msg.sender] -= _amount;
+    }
+
+    function tokenURI(uint256) public pure override returns (string memory) {
+        return "lmao, no uri";
+    }
+
+    function mint(address _recipient) external returns (uint256 newId) {
+        unchecked {
+            _mint(_recipient, (newId = ++totalSupply));
+        }
     }
 
     function mintFromGoo(uint256 maxPrice, bool useVirtualBalance)
